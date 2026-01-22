9e5fdcba72b7b4e26825a4aa0e1b46ce8c45c60d
grunhaus-holdings <grunhausholdings@gmail.com>
2026-01-22 15:18:49 -0500

    Completing the vercel fixes

--- .vercel/output/_functions/_@astrojs-ssr-adapter.mjs
+++
@@ -1,1 +0,0 @@
-export { c as createExports, a as start } from './assets/_@astrojs-ssr-adapter.Dzcmv6Vm.js';
--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../assets/generic@_@js.FR3nJFOq.js';
-export { renderers } from '../renderers.mjs';
